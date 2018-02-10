7f2ca9331e22639fb05c96569b49d566d8574c67
Matthew Roberts <mroberts@marketstar.com>
2018-02-09 17:07:50 -0700

    initial commit

---
+++ pather/pather/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace pather
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var x = System.Environment.GetEnvironmentVariable("Path");
+			x = x + @"";
+			Environment.SetEnvironmentVariable("Path", x, EnvironmentVariableTarget.Machine);
+			Console.WriteLine("Hello World!");
+
+			//-p path to destination
+		}
+	}
+}
